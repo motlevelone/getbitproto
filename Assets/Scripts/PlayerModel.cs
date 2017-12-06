10dc34e377d3b6bee87a0b73bf9b99becbaf5822
Timotius Prihadi <thedarkmot@gmail.com>
2017-12-07 06:37:53 +0700

    Start importing

---
+++ Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour {
+    public PlayerColor playerColor;
+    public Animator anim;
+    public Material[] materials;
+    public GameObject[] limbs;
+    public SkinnedMeshRenderer[] models;
+    List<int> limbsIndex;
+
+    void OnEnable()
+    {
+        SetSwim();
+        limbsIndex = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            limbsIndex.Add(i);
+            limbs[i].SetActive(true);
+        }
+        for (int i = 0; i < models.Length; i++)
+        {
+            models[i].material = materials[(int)playerColor];
+        }
+    }
+
+    public void SetSwim()
+    {
+        anim.SetInteger("State",0);
+    }
+
+    public void SetWin()
+    {
+        anim.SetInteger("State", 1);
+    }
+
+    public void GetBit()
+    {
+        anim.SetInteger("State",2);
+        if (limbsIndex.Count > 0)
+        {
+            int randomLimbs = Random.Range(0, limbsIndex.Count);
+            limbs[randomLimbs].SetActive(false);
+            limbsIndex.Remove(randomLimbs);
+        }
+
+    }
+}
